20ef48b9a0407c84d7a058ef7896a3d33a0e3fee
square <100358755+blumen1234@users.noreply.github.com>
2022-10-20 17:46:45 +0200

    Update Playground.java

--- Playground.java
+++ Playground.java
@@ -1,16 +1,23 @@
 package ch.fhgr.jenb.snake.modell;
 
 public class Playground {
-	private Grid[][] grids = new Grid[10][10];
+	
+	private Grid[][] grids; 
 	
 	public Playground() {
 		
-		
-	}
-public boolean eatGrid(int x, int y) {
+	    grids = new Grid[10][10];   
+	    // Anfangswerte setzen für alle 100 Felder   
+	    for (int i = 0; i < 10; i++) { 
+	      // senkrecht     
+	      for (int j = 0; j < 10; j++){ 
+	        //waagerecht
+	        grids[j][i]=new Grid(); 
 	
-	Grid grid = grids[x][y];
-	grid.eat();
-	return grid.isSnakebody();
-}
+		
+			}
+		}
+
+	}
+
 }
