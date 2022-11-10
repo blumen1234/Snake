22f2d01b686fdc94ecdcb92df7be966170b76555
square <100358755+blumen1234@users.noreply.github.com>
2022-11-10 12:03:43 +0100

    Update and rename Playground.java to Main.java

---
+++ Main.java
@@ -0,0 +1,40 @@
+package ch.fhgr.jenb.snake;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		testSnake();
+	}
+	
+
+	
+	public void testSnake() {
+		SnakeGrid brd = new SnakeGrid();
+		System.out.println(brd);
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+		
+	}
+}
+
