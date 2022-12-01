890bb276a294e617fb98345f06225006cc197d04
resin22 <101395463+resin22@users.noreply.github.com>
2022-12-01 09:12:43 +0100

    Update Game.java

--- Game.java
+++ Game.java
@@ -4,9 +4,8 @@ public class Game {
 	
 /* game init - ruft die Frucht auf und Gameover*/
 	
-/*Vererbung der Frucht*/
+/*Vererbung der Frucht / die Frucht taucht mit Random auf einem Feld auf*/
 
 
 }
-
 
