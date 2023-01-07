d1a3c447b2f20b7cad0cf57ef411199571613c4b
square <100358755+blumen1234@users.noreply.github.com>
2023-01-07 10:00:42 +0100

    Update View.java

--- View.java
+++ View.java
@@ -1,12 +1,9 @@
-package ch.fhgr.jenb.snake;
+package ch.fhgr.jenb.snake.main;
 
-
-
+import ch.fhgr.jenb.snake.main.logik.SnakeGrid;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.event.ActionEvent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
@@ -18,205 +15,154 @@ import javafx.scene.layout.Region;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
-
-// Funktion, um die Application abzuspielen
-
 public class Main extends Application {
 
-	// Membervariablen werden hier definiert
-
-
-	//Die Klassen aus dem File SnakeGrid werden in View integriert
+	private GridPane grid = new GridPane();
+	private Timeline timeline = new Timeline();
 	private SnakeGrid snakegrid = new SnakeGrid();
-	// Farben, Formen und Grössen werden hier für das Spielfeld definiert
-	private static final String style_raster = "-fx-background-color: grey; -fx-border-style: solid; -fx-border-width: 1; -fx-border-color: black; -fx-min-width: 60; -fx-min-height: 60; -fx-max-width:40; -fx-max-height: 40;";
-	private static final String style_snake = "-fx-background-color: green; -fx-border-style: solid; -fx-border-width: 1; -fx-border-color: black; -fx-min-width: 60; -fx-min-height:60; -fx-max-width:40; -fx-max-height: 40;";
-	private static final String style_apple = "-fx-background-color: red; -fx-border-style: solid; -fx-border-width: 1; -fx-border-color: black; -fx-min-width: 60; -fx-min-height:60; -fx-max-width:40; -fx-max-height: 40;";
-
-	// hier wird die Funktion via Import Java aufgerufen
-	GridPane grid = new GridPane();
 	
-	// Die maximale Länge und maximale Breite des Rasters wird hier als variable definiert
-	int rows = snakegrid.MAX_LENGTH;
-	int columns = snakegrid.MAX_LENGTH;
-	
-	private int speed = 500;
-	
-	// Funktion, um das Raster immer wieder zu zeichnen
-	public void snakeinterface() {
-		grid.getChildren().clear();
-		// die for-Schleife zeichnet immer wieder das Raster für das Spielfeld
-		for (int row = 0; row < rows; row++) {
-			for (int col = 0; col < columns; col++) {
-				Region r2 = new Region();
-				if (snakegrid.isSnakeOnfield(row, col)) {
-					r2.setStyle(style_snake);
 
-				} else if (snakegrid.isFruitOnfield(row, col)) {
-					r2.setStyle(style_apple);
-				} else
-					r2.setStyle(style_raster);
-				grid.add(r2, col, row);
-				
-			}
-		}
-		
-	}
+	private static final String STYLE_RASTER = "-fx-background-color: grey; -fx-border-style: solid; -fx-border-width: 1; -fx-border-color: black; -fx-min-width: 60; -fx-min-height: 60; -fx-max-width:40; -fx-max-height: 40;";
+	private static final String STYLE_SNAKE = "-fx-background-color: green; -fx-border-style: solid; -fx-border-width: 1; -fx-border-color: black; -fx-min-width: 60; -fx-min-height:60; -fx-max-width:40; -fx-max-height: 40;";
+	private static final String STYLE_APPLE = "-fx-background-color: red; -fx-border-style: solid; -fx-border-width: 1; -fx-border-color: black; -fx-min-width: 60; -fx-min-height:60; -fx-max-width:40; -fx-max-height: 40;";
 
+			
 	private KeyCode direction = KeyCode.DOWN;
+	private int SPEED = 800;
 
-	// Erstelle eine neue Timeline
-	Timeline timeline = new Timeline();
-				
-			
-	private void moveSnake() {
-	    // Bewegung der Schlange steuern
-	    switch (direction) {
-	        case UP:
-	    
-	        	snakegrid.snakeUp();
-	    	
-	        	snakeinterface();
-	        	
-	    
-	            // Schlange nach oben bewegen
-	            break;
-	        case DOWN:
-	    
-	        	snakegrid.snakedown();
-	    	
-	        	snakeinterface();
-	    	
-	            // Schlange nach unten bewegen
-	            break;
-	        case LEFT:
-	   
-	        	snakegrid.snakeleft();
-	    
-	        	snakeinterface();
-	            // Schlange nach links bewegen
-	            break;
-	        case RIGHT:
-	   
-	        	snakegrid.snakeright();
-	        	snakeinterface();
-	            // Schlange nach rechts bewegen
-	            break;
-	    }
-	    
-		if (snakegrid.isGameover()){
-			timeline.stop();
-			Alert meldung = new Alert(AlertType.INFORMATION);
-			meldung.setHeaderText("GAMEOVER");
-			meldung.setContentText("Du hast leider verloren");
-			meldung.showAndWait().ifPresent(ok -> {
-				
-			    if (ok == ButtonType.OK) {
-			    	snakegrid.toReset();
-			    	restartTimeline();
-			        System.out.println("OK gedrückt");
-			    }
-			
-			});	
-	}
-		
-	}
-
-	
-	private void Timeline() {
-		timeline.setCycleCount(Timeline.INDEFINITE);
-		// Erstelle ein KeyFrame, das die moveSnake()-Methode aufruft und setze die Dauer auf 500 Millisekunden
-		KeyFrame keyFrame = new KeyFrame(Duration.millis(speed), event -> moveSnake());
-		// Füge das KeyFrame der Timeline hinzu
-		timeline.getKeyFrames().add(keyFrame);
-		// Starte die Timeline
-		timeline.play();
-		
-	}
-	private void restartTimeline() {
-	    timeline.stop();
-	    timeline.getKeyFrames().clear();
-	    Timeline();
-	}
-	
 	@Override
 	public void start(Stage stage) throws Exception {
-		
+
 		Timeline();
-					
-		// Titel der Desktopanzeige
+
 		stage.setTitle("Snake");
-		
-		// ruft die Funktion im SnakeGrid auf
+
 		snakegrid.initGrid();
-		
-		// ruft die Funktion auf
 		snakeinterface();
-		
-		// In dem Desktop soll folgendes erscheinen, die Funktionen und die Grösse des Desktops
+
 		Scene scene = new Scene(grid, 600, 600);
-		// Anzeige des Desktops
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 		stage.setScene(scene);
-		
-		// zur Anzeige soll die Tastatur mitverwendet werden, keyEvent wird vom Import Java geholt
 		stage.getScene().addEventFilter(KeyEvent.KEY_PRESSED, e -> {
-			if (snakegrid.isGameover()){
-				timeline.stop();
-				Alert meldung = new Alert(AlertType.INFORMATION);
-				meldung.setHeaderText("GAMEOVER");
-				meldung.setContentText("Du hast leider verloren");
-				meldung.showAndWait().ifPresent(ok -> {
-			
-				    if (ok == ButtonType.OK) {
-				    	snakegrid.toReset();
-				    	restartTimeline();
-				        System.out.println("OK gedrückt");
-				    }
-				
-				});	
-		}
-			
-		
-			
+
 			if (e.getCode() == KeyCode.UP) {
-			
+
 				direction = KeyCode.UP;
 				moveSnake();
-				 System.out.println("up");
+				System.out.println("up");
 			}
 
 			else if (e.getCode() == KeyCode.LEFT) {
-		
+
 				direction = KeyCode.LEFT;
 				moveSnake();
 				System.out.println("left");
 			}
 
 			else if (e.getCode() == KeyCode.DOWN) {
-			
 				direction = KeyCode.DOWN;
 				moveSnake();
 				System.out.println("down");
 			}
 
 			else if (e.getCode() == KeyCode.RIGHT) {
-				 
 				direction = KeyCode.RIGHT;
 				moveSnake();
 				System.out.println("right");
 			}
-		
+
 		}
 
 		);
 		stage.show();
-		
+
 	}
 
+	public void snakeinterface() {
+		for (int row = 0; row < snakegrid.MAX_LENGTH; row++) {
+			for (int col = 0; col < snakegrid.MAX_LENGTH; col++) {
+				Region r2 = new Region();
+				if (snakegrid.isSnakeOnfield(row, col)) {
+					r2.setStyle(STYLE_SNAKE);
+
+				} else if (snakegrid.isFruitOnfield(row, col)) {
+					r2.setStyle(STYLE_APPLE);
+				} else
+					r2.setStyle(STYLE_RASTER);
+				grid.add(r2, col, row);
+
+			}
+		}
+
+	}
+
+	public void moveSnake() {
+
+		switch (direction) {
+		case UP:
+
+			snakegrid.snakeUp();
+
+			snakeinterface();
+
+			break;
+		case DOWN:
+
+			snakegrid.snakeDown();
+
+			snakeinterface();
+
+			break;
+		case LEFT:
+
+			snakegrid.snakeLeft();
+
+			snakeinterface();
+
+			break;
+		case RIGHT:
+
+			snakegrid.snakeRight();
+			snakeinterface();
+
+			break;
+		default:
+			break;
+		}
+
+		if (snakegrid.isGameover()) {
+			timeline.stop();
+			ButtonType okButton = new ButtonType("OK");
+			Alert alert = new Alert(AlertType.INFORMATION, "Game Over!", okButton);
+			alert.show();
+			alert.setOnCloseRequest(event -> {
+				ButtonType result = alert.getResult();
+				if (result == okButton) {
+					snakegrid.toReset();
+					restartTimeline();
+					System.out.println("gameover");
+				}
+			});
+		}
+
+	}
+
+	private void Timeline() {
+		timeline.setCycleCount(Timeline.INDEFINITE);
+		KeyFrame keyFrame = new KeyFrame(Duration.millis(SPEED), event -> moveSnake());
+		timeline.getKeyFrames().add(keyFrame);
+		timeline.play();
+
+	}
+
+	private void restartTimeline() {
+		timeline.stop();
+		timeline.getKeyFrames().clear();
+		Timeline();
+	}
 
 	public static void main(String[] args) {
 		launch(args);
 
 	}
 }
-
